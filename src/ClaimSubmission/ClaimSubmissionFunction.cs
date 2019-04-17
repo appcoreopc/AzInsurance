8fb5ced263b4754b5809656f1ade4696abf3652c
jeremy <jeremy.woo@c3posttrade.com>
2019-04-18 09:01:25 +1200

    group projects into a solution file.

--- src/ClaimSubmission/ClaimSubmissionFunction.cs
+++ src/ClaimSubmission/ClaimSubmissionFunction.cs
@@ -18,7 +18,7 @@ namespace ClaimSubmission
     {
         [FunctionName("ClaimSubmissionFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log, ExecutionContext context)
         {
             var config = BuildAppConfig(context);
