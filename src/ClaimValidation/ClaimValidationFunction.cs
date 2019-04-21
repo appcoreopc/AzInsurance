564720f320f63bb20fa0f45a959e82d32f558564
jeremy woo <kepungmoin@hotmail.com>
2019-04-22 07:14:17 +1200

    updated function to http triggered function

--- src/ClaimValidation/ClaimValidationFunction.cs
+++ src/ClaimValidation/ClaimValidationFunction.cs
@@ -1,15 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace ClaimValidation
 {
     public static class ClaimValidationFunction
     {
         [FunctionName("ClaimValidationFunction")]
-        public static void Run([ServiceBusTrigger("myqueue", Connection = "")]string myQueueItem, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ILogger log, ExecutionContext context)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.LogInformation($"Validating claim");
+            await ValidateClaimData();
+
+            return (ActionResult) new OkObjectResult("");          
+        }
+
+
+        private static async Task ValidateClaimData() {
+
+            await Task.Delay(1000);
         }
     }
 }
