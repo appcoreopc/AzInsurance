4a94cb7863808ee4497650233098265158ad7f00
JeremyWoo <jeremy.woo@c3posttrade.com>
2019-04-23 11:44:46 +1200

    added queue logging info

--- src/ClaimValidation/ClaimValidationFunction.cs
+++ src/ClaimValidation/ClaimValidationFunction.cs
@@ -12,17 +12,18 @@ namespace ClaimValidation
         public static void Run([ServiceBusTrigger(ApplicationConstants.TargetClaimQueueName, Connection = "Endpoint=sb://devservicespace.servicebus.windows.net/;SharedAccessKeyName=WritePolicy;SharedAccessKey=/qr6BPIcrll6WOWea85wwfqHWjllo8r1YR6uRv558V4=;")] string queueItem,
         Int32 deliveryCount, DateTime enqueuedTimeUtc, string messageId, ILogger log, ExecutionContext context)
         {
-            log.LogInformation($"Claim validation");
+            log.LogInformation($"Claim validation content:", queueItem);
             var config = CoreConfiguration.BuildAppConfig(context);
             var messageConnectionString = config[ApplicationConstants.StorageConnectionString];
-
             var userClaimFormData = MessageConverter.Deserialize<ClaimForm>(queueItem);
             ProcessMessage(userClaimFormData);
         }
 
         private static void ProcessMessage(ClaimForm data)
         {
+            // Query database 
 
+            // 
         }
     }
 }
